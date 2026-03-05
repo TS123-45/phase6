4767030569433a2ee36fa8535d9a77321e6eafae
TS123-45 <mohanrajz8181@gmail.com>
2026-03-05 20:03:05 +0530

    login.js

--- phase6/login.js
+++ phase6/login.js
@@ -1,51 +1,56 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const pool = require("./db");
+const CustomError = require("./utils/CustomError");
 
 const router = express.Router();
 
-// ---------- SIGNUP or CREATE A ACCOUNT----------
-router.post("/signup", async (req, res) => {
+// SIGNUP
+router.post("/signup", async (req, res, next) => {
   const { username, password } = req.body;
 
   if (!username || !password) {
-    return res.status(400).json({ message: "All fields required" });
+    return next(new CustomError("All fields required", 400));
   }
 
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+
     pool.query(sql, [username, hashedPassword], (err) => {
       if (err) {
-        return res.status(500).json({ error: err.message });
+        return next(new CustomError("username already exists", 500));
       }
+
       res.status(201).json({ message: "Signup successful" });
     });
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    next(err);
   }
 });
 
-// ---------- LOGIN ----------
-router.post("/login", (req, res) => {
+// LOGIN
+router.post("/login", (req, res, next) => {
   const { username, password } = req.body;
 
-  const sql = "SELECT * FROM users WHERE username = ?";
+  const sql = "SELECT * FROM users WHERE username=?";
+
   pool.query(sql, [username], async (err, results) => {
     if (err) {
-      return res.status(500).json({ error: err.message });
+      return next(new CustomError("Database error", 500));
     }
 
     if (results.length === 0) {
-      return res.status(401).json({ message: "User not found" });
+      return next(new CustomError("User not found", 401));
     }
 
     const user = results[0];
+
     const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
-      return res.status(401).json({ message: "Invalid password" });
+      return next(new CustomError("Invalid password", 401));
     }
 
     res.json({ message: "Login successful" });
